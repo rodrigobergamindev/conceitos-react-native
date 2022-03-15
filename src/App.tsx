99a2456741946f7579c1461d84bc336c28f7305f
rodrigobergamindev <rodrigobergamindev@gmail.com>
2022-03-15 20:22:06 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
+import {View} from 'react-native';
+import style from './components/First/style/style';
+import React from 'react';
 
-
-import { View, SafeAreaView, Text } from 'react-native'
-import style from './components/First/style/style'
-import React from 'react'
-import First from './components/First/First'
-import MinMax from './components/MinMax/MinMax'
+import MinMax from './components/MinMax/MinMax';
 
 const App = () => {
   return (
     <View style={style.App}>
-        {/*<First/>*/}
-        <MinMax x={3} y={9}/>
+      <MinMax x={3} y={9} />
     </View>
-  )
-}
+  );
+};
 
-
-
-
-export default App
+export default App;
---
+++ src/components/Aleatory/Aleatory.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function Aleatory() {
+  return (
+    <View>
+      <Text>Aleatory</Text>
+    </View>
+  )
+}
